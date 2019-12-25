ddc810dee7912936580a7022c18b34eed30f6b20
Brian Hunt <brianmhunt@gmail.com>
2019-12-25 12:19:17 -0500

    move firebase.json to right directory, remove dupe firebase install

--- run.sh
+++ run.sh
@@ -2,9 +2,5 @@
 set -x
 
 echo "Hello world."
-npm i firebase
 
-echo "CLO?UD CREDS:: $GOOGLE_APPLICATION_CREDENTIALS / $FIREBASE_TOKEN"
-
-npx firebase login
 npx firebase deploy --project minutebox-labs --only hosting --debug
